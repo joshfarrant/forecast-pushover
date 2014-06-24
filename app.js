ba02061dcfdeb5bdace0c0c0d319b16ded10251a
Josh Farrant <josh.farrant@sipcentric.com>
2014-06-24 17:31:06 +0100

    Added wait time between notifications, can now only send 1 every 10 minutes

--- app.js
+++ app.js
@@ -33,16 +33,19 @@ function getReadableIntensity(intensity) {
   return response;
 };
 
+var lastNotificationTime = 0;
+
 // Checks forecast.io API periodically for up-to-date weather data
 setInterval(function() {
-
+  console.log('tick');
   // Gets current weather data from forecast.io API
   request(forecastURL, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var bodyParsed = JSON.parse(body);
       var nextHour = bodyParsed['minutely']['data'];
-      var minPrecipProbability = 0.07;
+      var minPrecipProbability = 0.0007;
       var currentTime = bodyParsed['currently']['time'];
+      var notificationWaitTime = 600;
 
       // Builds array of all times when precipitation probability is greater than the specified minimum
       for (var i in nextHour) {
@@ -62,13 +65,19 @@ setInterval(function() {
         var minutesToPrecip = (secondsToPrecip / 60) | 0;
         var intensity = getReadableIntensity(upcomingPrecip[0]['precipIntensity']);
 
+        console.log(nextPrecip);
+
         var title = intensity + nextPrecip['precipType'];
         var message = "Starting in " + minutesToPrecip + " minutes.";
 
-        console.log("Notification sent:")
-        console.log(title);
-        console.log(message);
-        sendNotification(title, message);
+        if (currentTime >= (lastNotificationTime + notificationWaitTime)) {
+          console.log("Notification sent:")
+          console.log(title);
+          console.log(message);
+          sendNotification(title, message);
+
+          lastNotificationTime = currentTime;
+        };
       };
     };
   });
