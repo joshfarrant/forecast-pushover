04f80d3d5f946c541ad11446c43e378fa23e43e5
Josh Farrant <josh.farrant@sipcentric.com>
2014-06-24 17:07:18 +0100

    Getting hourly data from Forecast.io every minute, then sending a notification if precipitation is forecast

--- app.js
+++ app.js
@@ -1,27 +1,75 @@
 var Pushover = require('node-pushover');
+var http = require('http');
 var fs = require('fs');
+var request = require('request');
 
-//Reads config.json to get API token and user key
-fs.readFile("config.json", function (err, data) {
-  if (err) {
-    console.log('Error: ' + err);
-  }
-  var content = JSON.parse(data);
-  token = content["token"];
-  user = content["user"];
-  sendNotification(token, user)
-});
+// Reads config.json to get credentials for Pushover and Forecast.io APIs
+var credentials = JSON.parse(fs.readFileSync('config.json'));
+var pushoverToken = credentials['pushoverToken'];
+var pushoverUser = credentials['pushoverUser'];
+var forecastKey = credentials['forecastKey'];
+var forecastURL = "https://api.forecast.io/forecast/" + forecastKey + "/52.4782600,-1.8944970?exclude=flags,alerts,daily,hourly&units=si";
 
-//Sends the notification
-function sendNotification(token, user) {
+// Sends the notifications
+function sendNotification(title, message) {
   var push = new Pushover({
-    token: token,
-    user:  user
+    token: pushoverToken,
+    user:  pushoverUser
   });
-
-  var title =   "Oh...";
-  var message = "Hello!!!"
-
   push.send(title, message);
 };
 
+// Converts intensity into a readable format
+function getReadableIntensity(intensity) {
+  if (intensity < 0.432) {
+    response = "Very light ";
+  } else if (intensity < 2.54) {
+    response = "Light ";
+  } else if (intensity < 10.16) {
+    response = "Moderate ";
+  } else if (intensity >= 10.16) {
+    response = "Heavy ";
+  };
+  return response;
+};
+
+// Checks forecast.io API periodically for up-to-date weather data
+setInterval(function() {
+
+  // Gets current weather data from forecast.io API
+  request(forecastURL, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var bodyParsed = JSON.parse(body);
+      var nextHour = bodyParsed['minutely']['data'];
+      var minPrecipProbability = 0.07;
+      var currentTime = bodyParsed['currently']['time'];
+
+      // Builds array of all times when precipitation probability is greater than the specified minimum
+      for (var i in nextHour) {
+        var precipProbability = nextHour[i]['precipProbability'];
+        if (precipProbability >= minPrecipProbability) {
+          if (!upcomingPrecip) {
+            var upcomingPrecip = [];
+          };
+          upcomingPrecip.push(nextHour[i]);
+        };
+      };
+      
+      // Builds and sends notification if there is precipitation within the next hour
+      if (upcomingPrecip) {  
+        var nextPrecip = upcomingPrecip[0];
+        var secondsToPrecip = (upcomingPrecip[0]['time'] - currentTime);
+        var minutesToPrecip = (secondsToPrecip / 60) | 0;
+        var intensity = getReadableIntensity(upcomingPrecip[0]['precipIntensity']);
+
+        var title = intensity + nextPrecip['precipType'];
+        var message = "Starting in " + minutesToPrecip + " minutes.";
+
+        console.log("Notification sent:")
+        console.log(title);
+        console.log(message);
+        sendNotification(title, message);
+      };
+    };
+  });
+}, 60000);
