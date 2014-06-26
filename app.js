2ae6e9b1fd53e70e0f9a537c1c8a45cfb061746d
Josh Farrant <josh.farrant@sipcentric.com>
2014-06-26 08:54:55 +0100

    Prevented API calls and notifications from being sent early in the morning and late at night

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function sendNotification(title, message) {
     user:  pushoverUser
   });
   push.send(title, message);
-  lastNotificationTime = currentTime;
+  lastNotificationTime = lastRequestTime;
   return lastNotificationTime;
 };
 
@@ -46,86 +46,95 @@ function getReadableIntensityAndPriority(intensity) {
 };
 
 var lastNotificationTime = 0;
+var currentDate = new Date();
+var currentDay = currentDate.getDay();
+var currentTimestamp = (currentDate.getTime() / 1000) | 0;
+var currentHour = currentDate.getHours();
 
-// Checks forecast.io API periodically for up-to-date weather data
-setInterval(function() {
-  console.log('tick');
-  // Gets current weather data from forecast.io API
-  request(forecastURL, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var bodyParsed = JSON.parse(body);
-      var nextHour = bodyParsed['minutely']['data']; // All minutely weather data for the next hour
-      var minPrecipProbability = 0.0007; // Minimum precipitation probability to class as weather event
-      var currentPrecip = false;
-      var currentTime = bodyParsed['currently']['time']; // Gets current time from response
-      var notificationWaitTime = 1200; // Time to wait between sending notifications
+// Prevents API calls and notifications from going out early in the morning or late at night
+if (currentHour >= 8 && currentHour < 21) {
 
-      if (bodyParsed['currently']['precipType']) {
-        currentPrecip = bodyParsed['currently']['precipType'];
-      };
+  // Checks forecast.io API periodically for up-to-date weather data
+  setInterval(function() {
 
-      var precipProbability;
-      var previousPrecip = false;
+    console.log('tick');
+    // Gets current weather data from forecast.io API
+    request(forecastURL, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var bodyParsed = JSON.parse(body);
+        var nextHour = bodyParsed['minutely']['data']; // All minutely weather data for the next hour
+        var minPrecipProbability = 0.0007; // Minimum precipitation probability to class as weather event
+        var currentPrecip = false;
+        var lastRequestTime = bodyParsed['currently']['time']; // Gets last request time from response
+        var notificationWaitTime = 1200; // Time to wait between sending notifications
 
-      // Gets next time when precipitation probability is greater than the specified minimum
-      // If this precipitation will last more than 1 min, builds an array of all items in the next precipitation event
-      // Stops building array if a precipitation event stops
-      for (var i in nextHour) {
-        precipProbability = nextHour[i]['precipProbability'];
-        if (precipProbability >= minPrecipProbability) {
-          if (!upcomingPrecip) {
-            var upcomingPrecip = [];
+        if (bodyParsed['currently']['precipType']) {
+          currentPrecip = bodyParsed['currently']['precipType'];
+        };
+
+        var precipProbability;
+        var previousPrecip = false;
+
+        // Gets next time when precipitation probability is greater than the specified minimum
+        // If this precipitation will last more than 1 min, builds an array of all items in the next precipitation event
+        // Stops building array if a precipitation event stops
+        for (var i in nextHour) {
+          precipProbability = nextHour[i]['precipProbability'];
+          if (precipProbability >= minPrecipProbability) {
+            if (!upcomingPrecip) {
+              var upcomingPrecip = [];
+            };
+            upcomingPrecip.push(nextHour[i]);
+            previousPrecip = true;
+          } else if (previousPrecip == true) {
+            break;
           };
-          upcomingPrecip.push(nextHour[i]);
-          previousPrecip = true;
-        } else if (previousPrecip == true) {
-          break;
         };
-      };
 
-      // Builds and sends notification if there is precipitation within the next hour
-      if (upcomingPrecip && currentPrecip == false) {  
-        var nextPrecip = upcomingPrecip[0];
-        var secondsToPrecip = (upcomingPrecip[0]['time'] - currentTime);
-        var minutesToPrecip = (secondsToPrecip / 60) | 0;
+        // Builds and sends notification if there is precipitation within the next hour
+        if (upcomingPrecip && currentPrecip == false) {  
+          var nextPrecip = upcomingPrecip[0];
+          var secondsToPrecip = (upcomingPrecip[0]['time'] - lastRequestTime);
+          var minutesToPrecip = (secondsToPrecip / 60) | 0;
 
-        var intensityAndPriority = getReadableIntensityAndPriority(upcomingPrecip[0]['precipIntensity']);
-        var intensity = intensityAndPriority[0];
-        var priority = intensityAndPriority[1];
-        var message = "";
-        var nextPrecipDuration = 0; // How many minutes the next precipitation will occur for
+          var intensityAndPriority = getReadableIntensityAndPriority(upcomingPrecip[0]['precipIntensity']);
+          var intensity = intensityAndPriority[0];
+          var priority = intensityAndPriority[1];
+          var message = "";
+          var nextPrecipDuration = 0; // How many minutes the next precipitation will occur for
 
-        var title = intensity + nextPrecip['precipType'];
-        if (minutesToPrecip != 0){
-          message = "Starting in " + minutesToPrecip + " minutes.";
-        } else {
-          message = "Starting now.";
-        };
-        var notificationLimitExpiry = lastNotificationTime + notificationWaitTime;
-
-        // Checks priority of upcoming weather event
-        // Low or medium priority event notifications are limited by the notificationWaitTime
-        // High priority events are not limited
-        if (priority == "Low" || priority == "Medium") {
-          if (currentTime >= notificationLimitExpiry) {
-            console.log("Notification sent:")
-            console.log(title);
-            console.log(message);
-            console.log(priority);
-
-            // lastNotificationTime = sendNotification(title, message);
+          var title = intensity + nextPrecip['precipType'];
+          if (minutesToPrecip != 0){
+            message = "Starting in " + minutesToPrecip + " minutes.";
           } else {
-            console.log("Notification limit reached, limit resets in " + (currentTime - notificationLimitExpiry) + " seconds")
+            message = "Starting now.";
           };
-        } else if (priority == "High") {
-            console.log("Notification sent:")
-            console.log(title);
-            console.log(message);
-            console.log(priority);
+          var notificationLimitExpiry = lastNotificationTime + notificationWaitTime;
 
-            // lastNotificationTime = sendNotification(title, message);
+          // Checks priority of upcoming weather event
+          // Low or medium priority event notifications are limited by the notificationWaitTime
+          // High priority events are not limited
+          if (priority == "Low" || priority == "Medium") {
+            if (lastRequestTime >= notificationLimitExpiry) {
+              console.log("Notification sent:")
+              console.log(title);
+              console.log(message);
+              console.log(priority);
+
+              // lastNotificationTime = sendNotification(title, message);
+            } else {
+              console.log("Notification limit reached, limit resets in " + (lastRequestTime - notificationLimitExpiry) + " seconds")
+            };
+          } else if (priority == "High") {
+              console.log("Notification sent:")
+              console.log(title);
+              console.log(message);
+              console.log(priority);
+
+              // lastNotificationTime = sendNotification(title, message);
+          };
         };
       };
-    };
-  });
-}, 180000);
+    });
+  }, 180000);
+};
